6275f027ecd80a6f3a1cabc14b956c55c61a1093
jibekabdykerim <254720@astanait.edu.kz>
2026-03-24 20:20:57 +0500

    updated the code wrote it all together

--- src/main/java/app/Assignment1.java
+++ src/main/java/app/Assignment1.java
@@ -1,14 +1,69 @@
 package app;
 
 public class Assignment1 {
+
+    //Task1
     public void printDigits(int n) {
-        if(n==0){
-            return;
-        }
-        printDigits(n/10);
+        if (n == 0) return; //base case
+        printDigits(n / 10); //recursive call
         System.out.println(n % 10);
     }
 
+    //Task2
+    public double calculateSum(int[] arr, int n) {
+        if (n <= 0) return 0; //base case
+        return arr[n - 1] + calculateSum(arr, n - 1); //recursive call
+    }
 
+    // Task3
+    public boolean isPrime(int n, int i) {
+        if (n <= 2) return (n == 2); //base case
+        if (n % i == 0) return false;
+        if (i * i > n) return true;
+        return isPrime(n, i + 1); //recursive call
+    }
 
+    //Task4
+    public int factorial(int n) {
+        if (n <= 1) return 1; //base case
+        return n * factorial(n - 1); //recursive call
+    }
+
+    // Task5
+    public int fibonacci(int n) {
+        if (n <= 1) return n; //base case
+        return fibonacci(n - 1) + fibonacci(n - 2); //recursive call
+    }
+
+    //Task6
+    public int power(int a, int n) {
+        if (n == 0) return 1; //base case
+        return a * power(a, n - 1); //recursive call
+    }
+
+    //Task7
+    public void reversePrint(int[] arr, int n) {
+        if (n <= 0) return; //base case
+        System.out.print(arr[n - 1] + " ");
+        reversePrint(arr, n - 1); //recursive call
+    }
+
+    //Task8
+    public boolean isNumeric(String s) {
+        if (s.length() == 0) return true; // Base case
+        if (!Character.isDigit(s.charAt(0))) return false; // Base case
+        return isNumeric(s.substring(1)); // Recursive call
+    }
+
+    //Task9
+    public int countChars(String s) {
+        if (s.equals("")) return 0; // Base case
+        return 1 + countChars(s.substring(1)); // Recursive call
+    }
+
+    //Task10
+    public int gcd(int a, int b) {
+        if (b == 0) return a; //bse case
+        return gcd(b, a % b); //recursive call
+    }
 }
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,13 +1,45 @@
 package app;
-import java.util.Scanner;
-
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
         Assignment1 task = new Assignment1();
-        System.out.print("Enter a number for task 1 ");
-        int num = sc.nextInt();
-        task.printDigits(num);
+
+        System.out.println(" Task1");
+        task.printDigits(5481);
+
+        System.out.println("\nTask2 ");
+        int[] arr2 = {3, 2, 4, 1};
+        System.out.println(task.calculateSum(arr2, 4) / 4);
+
+        System.out.println("\nTask3");
+        System.out.println("7 is: " + (task.isPrime(7, 2) ? "Prime" : "Composite"));
+        System.out.println("10 is: " + (task.isPrime(10, 2) ? "Prime" : "Composite"));
+
+        System.out.println("\nTask4");
+        System.out.println(task.factorial(5));
+
+        System.out.println("\nTask5");
+        System.out.println("F5: " + task.fibonacci(5));
+        System.out.println("F17: " + task.fibonacci(17));
+
+        System.out.println("\nTask6");
+        System.out.println(task.power(2, 10));
+
+        System.out.println("\nTask7");
+        int[] arr7 = {1, 4, 6, 2};
+        task.reversePrint(arr7, 4);
+        System.out.println();
+
+        System.out.println("\n Task8");
+        System.out.println("123456: " + (task.isNumeric("123456") ? "Yes" : "No"));
+        System.out.println("123a12: " + (task.isNumeric("123a12") ? "Yes" : "No"));
+
+        System.out.println("\nTask9");
+        System.out.println("hello: " + task.countChars("hello"));
+        System.out.println("recursion: " + task.countChars("recursion"));
+
+        System.out.println("\nTask10");
+        System.out.println("GCD(32, 48): " + task.gcd(32, 48));
+        System.out.println("GCD(10, 7): " + task.gcd(10, 7));
     }
 }
